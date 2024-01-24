ca858eed755484ea5349acc24ad0059b2f68bd67
Andriy1years <andricho1905@gmail.com>
2024-01-24 20:20:05 +0200

    24 01 24

--- js/left_panel_button.js
+++ js/left_panel_button.js
@@ -1,43 +1,64 @@
-const left_panels = [
-    button_home = document.getElementById('button_home'),
-    soon = document.getElementById('soon'),
-    for_me = document.getElementById('for_me'),
-    admin_panel = document.getElementById('admin_panel'),
-];
+const button_home = document.getElementById('button_home');
+const button_soon = document.getElementById('button_soon');
+const button_for_me = document.getElementById('button_for_me');
+const button_admin_panel = document.getElementById('button_admin_panel');
+const home = document.querySelector('.home');
+const soon = document.querySelector('.soon');
+const for_me = document.querySelector('.for_me');
+const admin_panel = document.querySelector('.admin_panel');
+
+//left element
 
 
 button_home.addEventListener('click', () => {
     button_home.style.borderColor = '#c3c3c360';
-    soon.style.borderColor = '#c3c3c300';
-    for_me.style.borderColor = '#c3c3c300';
-    admin_panel.style.borderColor = '#c3c3c300';
+    button_soon.style.borderColor = '#c3c3c300';
+    button_for_me.style.borderColor = '#c3c3c300';
+    button_admin_panel.style.borderColor = '#c3c3c300';
+
+    home.style.display = 'block';
+    soon.style.display = 'none';
+    for_me.style.display = 'none';
+    admin_panel.style.display = 'none';
 });
 
-soon.addEventListener('click', () => {
+button_soon.addEventListener('click', () => {
     button_home.style.borderColor = '#c3c3c300';
-    soon.style.borderColor = '#c3c3c360';
-    for_me.style.borderColor = '#c3c3c300';
-    admin_panel.style.borderColor = '#c3c3c300';
+    button_soon.style.borderColor = '#c3c3c360';
+    button_for_me.style.borderColor = '#c3c3c300';
+    button_admin_panel.style.borderColor = '#c3c3c300';
 
+    home.style.display = 'none';
+    soon.style.display = 'block';
+    for_me.style.display = 'none';
+    admin_panel.style.display = 'none';
 });
 
-for_me.addEventListener('click', () => {
+button_for_me.addEventListener('click', () => {
     button_home.style.borderColor = '#c3c3c300';
-    soon.style.borderColor = '#c3c3c300';
-    for_me.style.borderColor = '#c3c3c360';
-    admin_panel.style.borderColor = '#c3c3c300';
+    button_soon.style.borderColor = '#c3c3c300';
+    button_for_me.style.borderColor = '#c3c3c360';
+    button_admin_panel.style.borderColor = '#c3c3c300';
 
+    home.style.display = 'none';
+    soon.style.display = 'none';
+    for_me.style.display = 'block';
+    admin_panel.style.display = 'none';
 });
 
-admin_panel.addEventListener('click', () => {
+button_admin_panel.addEventListener('click', () => {
     button_home.style.borderColor = '#c3c3c300';
-    soon.style.borderColor = '#c3c3c300';
-    for_me.style.borderColor = '#c3c3c300';
-    admin_panel.style.borderColor = '#c3c3c360';
+    button_soon.style.borderColor = '#c3c3c300';
+    button_for_me.style.borderColor = '#c3c3c300';
+    button_admin_panel.style.borderColor = '#c3c3c360';
 
+    home.style.display = 'none';
+    soon.style.display = 'none';
+    for_me.style.display = 'none';
+    admin_panel.style.display = 'block';
 });
 
 
 
-//------------------integration-----------------
+//--------title element--------//
 
