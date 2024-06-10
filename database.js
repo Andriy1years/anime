eb0a58334761923a3fd7a6fcf94d9a4e75a94a3a
Andriy1years <144339667+Andriy1years@users.noreply.github.com>
2024-06-10 12:08:21 +0300

    Add SQLite database support and fix issues

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-
+// Путь к базе данных
 const dbPath = path.resolve(__dirname, 'database.db');
 
-
+// Подключение к базе данных
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
@@ -21,4 +21,3 @@ const db = new sqlite3.Database(dbPath, (err) => {
 });
 
 module.exports = db;
-
