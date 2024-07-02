3345ddb85843c3cc57825e23bdd88255a5d93f29
Andriy1years <144339667+Andriy1years@users.noreply.github.com>
2024-07-02 19:59:03 +0300

    server

--- index.js
+++ index.js
@@ -1,74 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const connection = require('./database');
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Middleware для статических файлов
-app.use(express.static('public'));
 
-// Подключение JSON парсера для обработки POST запросов
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Маршрут для получения всех записей из таблицы animes
-app.get('/animes', (req, res) => {
-  const query = 'SELECT * FROM animes';
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error('Ошибка получения данных:', err);
-      res.status(500).send('Ошибка получения данных');
-      return;
-    }
-    res.json(results);
-  });
-});
-
-// Маршрут для добавления новой записи в таблицу animes
-app.post('/animes', (req, res) => {
-  const { name, url, img, main } = req.body;
-  const query = 'INSERT INTO animes (name, url, img, main) VALUES (?, ?, ?, ?)';
-  connection.query(query, [name, url, img, main], (err, results) => {
-    if (err) {
-      console.error('Ошибка добавления данных:', err);
-      res.status(500).send('Ошибка добавления данных');
-      return;
-    }
-    res.status(201).send('Запись добавлена');
-  });
-});
-
-// Маршрут для обновления записи в таблице animes
-app.put('/animes/:id', (req, res) => {
-  const { id } = req.params;
-  const { name, url, img, main } = req.body;
-  const query = 'UPDATE animes SET name = ?, url = ?, img = ?, main = ? WHERE id = ?';
-  connection.query(query, [name, url, img, main, id], (err, results) => {
-    if (err) {
-      console.error('Ошибка обновления данных:', err);
-      res.status(500).send('Ошибка обновления данных');
-      return;
-    }
-    res.send('Запись обновлена');
-  });
-});
-
-// Маршрут для удаления записи из таблицы animes
-app.delete('/animes/:id', (req, res) => {
-  const { id } = req.params;
-  const query = 'DELETE FROM animes WHERE id = ?';
-  connection.query(query, [id], (err, results) => {
-    if (err) {
-      console.error('Ошибка удаления данных:', err);
-      res.status(500).send('Ошибка удаления данных');
-      return;
-    }
-    res.send('Запись удалена');
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
--- public/js/apload_information.js
+++ public/js/apload_information.js
@@ -1,104 +1,104 @@
+document.addEventListener('DOMContentLoaded', () => {
+
 let pages = [
     //main
     {
         name: "Этот глупый свин не понимает мечту девочки-зайки",
         URL: "https://www.anilibria.tv/release/seishun-buta-yaro-wa-yumemiru-shoujo-no-yume-wo-minai.html",
-        img: "img/Этот глупый свин не понимает мечту девочки-зайки (Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai).jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/7462/2b7CtR3MKo58LTRX__e292c0f8c7b53aa42640222125b362b3.jpg",
+        main: 1
     },
 
     {
         name: "Небесное вторжение",
         URL: "https://www.anilibria.tv/release/tenkuu-shinpan.html",
         img: "img/Небесное вторжения.jpg",
-        main: true
+        main: 1
     },
 
     {
         name: "Заботливая 800-летняя жена",
         URL: "https://anilibria.best/online/zabotlivaja-800-letnjaja-zhena-sezon-1-2019-1080-hd/10-1-0-600",
         img: "img/senko.webp",
-        main: true
+        main: 1
     },
     {
         name: "Кейджо!!!!!!",
         URL: "https://anilibria.iss.ink/release/keijo.html",
-        img: "img/Кейджо!!!!!!.jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/3054/G0iufkrgQspQIYwI__ee1e950204800815d543f824de568e6c.jpg",
+        main: 1
     },
     {
         name: "Сто девушек, которые очень-очень-очень-очень-очень сильно тебя любят",
         URL: "https://www.anilibria.tv/release/kimi-no-koto-ga-daidaidaidaidaisuki-na-100-nin-no-kanojo.html",
-        img: "img/Сто девушек, которые очень-очень-очень-очень-очень сильно тебя любят.jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/9544/zhysjGOdJ4sTs2gZ__2c907d391ce33e6cd892c539f3702b39.jpg",
+        main: 1
     },
     {
         name: "Чёрный призыватель",
         URL: "https://www.anilibria.tv/release/kuro-no-shoukanshi.html",
-        img: "img/черный призыватель.jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/9222/H85Z4rMKKlQRBiam__c9111a3e8e186bf1b8222737a9390ac5.jpg",
+        main: 1
     },
     {
         name: "Я делаю всё возможное, чтобы чувствовать себя как дома в другом мире",
         URL: "https://www.anilibria.tv/release/isekai-de-mofumofu-nadenade-suru-tame-ni-ganbattemasu.html",
-        img: "img/Я делаю всё возможное, чтобы чувствовать себя как дома в другом мире.jpg",
-        ingorm: true,
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/9618/6XpIcbANM5xBa6t3__9532738d6f3f9a58245a696cee1753fb.jpg",
+        main: 1
     },
     {
-        name: "Семья шпионов",
+        name: "Семья шпиона",
         URL: "https://anilibria.zone/online/semja-shpiona-2022-1080-hd/10-1-0-1634",
-        img: "img/семья шпионов.jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/9161/112N0cfCJOqxzhMg__603664addb3416170efd513609bf83cb.jpg",
+        main: 1
     },
     {
         name: "Злодейка 99-го уровня",
         URL: "https://www.anilibria.tv/release/akuyaku-reijou-level-99-watashi-wa-ura-boss-desu-ga-maou-dewa-arimasen.html",
         img: "img/злодейка 99-го уровня.jpg",
-        main: true
+        main: 1
     },
     {
         name: "Мой телохранитель",
         URL: "https://www.anilibria.tv/release/ojou-to-banken-kun.html",
-        img: "img/Мой телохранитель.jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/9561/zmsXkmzvmVhwbwxY__8fe968fa1d15957c16a7f2747c4425d4.jpg",
+        main: 1
     },
     {
         name: "Этот герой неуязвим, но очень осторожен",
         URL: "https://anilibria.zone/online/ehtot_geroj_neujazvim_no_ochen_ostorozhen_sezon_1_2019_1080_hd/10-1-0-256",
         img: "img/Этот герой неуязвим, но очень осторожен.jpg",
-        main: true
+        main: 1
     },
     {
         name: "Эта фарфоровая кукла влюбилась",
         URL: "https://www.anilibria.tv/release/sono-bisque-doll-wa-koi-wo-suru.html",
-        img: "img/Эта фарфоровая кукла влюбилась.jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/9122/qNbPeJITcPEKToGU__1916d5e2cf72d1e72fbde4aefadb8b33.jpg",
+        main: 1
     },
     {
         name: "салатник чудаков",
         URL: "https://www.anilibria.tv/release/henjin-no-salad-bowl.html",
-        img: "img/салатник чудаков.jpg",
-        ingorm: true,
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/9689/g8oc5KzlVdhRqOBo__364fecb0561e9d03a561a580eca62f8d.jpg",
+        main: 1
     },
     {
         name: "Неразумный ангел в танце с демоном",
         URL: "https://www.anilibria.tv/release/oroka-na-tenshi-wa-akuma-to-odoru.html",
-        img: "img/Неразумный ангел в танце с демоном.jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/9619/iO1IycS7PfyB5a96__1091b00356102f8e0defd2eb251c3ee5.jpg",
+        main: 1
     },
     {
         name: "МЭШЛ: Магия и мускулы",
         URL: "https://www.anilibria.tv/release/mashle.html",
-        img: "img/Магия и мускулы.jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/9416/m7sgKrzJ0ZIMn97l__34e3469f0dac6353d515cb081e47311a.jpg",
+        main: 1
     },
     {
         name: "Класс убийц",
         URL: "https://ru-7e439cdefda91c2e-fra.anilibria.sbs/release/ansatsu-kyoushitsu-klass-ubiyts.html",
-        img: "img/Класс убийц.jpg",
-        main: true
+        img: "https://www.anilibria.tv/storage/releases/posters/409/Xk8Lb5uUiL1TAZ5k__761829a36615ea84d8af65e3fa8d6b26.jpg",
+        main: 1
     },
 
 
@@ -110,106 +110,106 @@ let pages = [
     {
         name: "Ёко из Сэнгоку",
         URL: "https://www.anilibria.tv/release/sengoku-youko.html",
-        img: "img/Ёко из Сэнгоку.jpg",
-        main: false
+        img: "https://www.anilibria.tv/storage/releases/posters/9634/yYYXMt50oM75YNHB__d6bab6b3b042d48a551054a2c6e15687.jpg",
+        main: 3
     },
     {
         name: "Заботы запертой принцессы вампиров",
         URL: "https://www.anilibria.tv/release/hikikomari-kyuuketsuki-no-monmon.html",
-        img: "img/заботы запертой принцессы вампиров.jpg",
-        main: false
+        img: "https://www.anilibria.tv/storage/releases/posters/9552/jR0JAdkgenbI2tsc__224485904f24d944e6b0a6dbf9332d29.jpg",
+        main: 3
     },
     {
         name: "Рагна Багровый",
         URL: "https://www.anilibria.tv/release/ragna-crimson.html",
-        img: "img/Рагна Багровый.jpg",
-        main: false
+        img: "https://www.anilibria.tv/storage/releases/posters/9554/reQYxCCOShMJdZd1__ac0b79dc25b18339177cf11bad95ebd2.jpg",
+        main: 3
     },
     {
         name: "Провожающая в последний путь Фрирен",
         URL: "https://www.anilibria.tv/release/sousou-no-frieren.html",
-        img: "img/Провожающая в последний путь Фрирен.jpg",
-        main: false
+        img: "https://www.anilibria.tv/storage/releases/posters/9542/R1ep1mdSXtUsAhgD__cf83260f7c4329bd4a768fefb4bedd25.jpg",
+        main: 3
     },
     {
         name: "Небо в цвету!",
         URL: "https://www.anilibria.tv/release/appare-ranman.html",
-        img: "img/Небо в цвету!.jpg",
-        main: false
+        img: "https://www.anilibria.tv/storage/releases/posters/8589/ek1Cm7HPQkya6gXj__9c310c923ff2e02aa03c6d5717edb4c0.jpg",
+        main: 3
     },
 
     {
         name: "Перевоплотился в седьмого принца, так что я буду совершенствовать свою магию, как захочу",
         URL: "https://www.anilibria.tv/release/tensei-shitara-dainana-ouji-datta-node-kimama-ni-majutsu-wo-kiwamemasu.html",
         img: "img/Перевоплотился в седьмого принца, так что я буду совершенствовать свою магию, как захочу.jpg",
-        main: false
+        main: 3
     },
     {
         name: "Дюрарара!!",
         URL: "https://www.anilibria.tv/release/durarara.html",
         img: "img/Дюрарара!!.jpg",
-        main: false
+        main: 3
     },
     {
         name: "Дюрарара!! x2 / DRR 2",
         URL: "https://www.anilibria.tv/release/drr-x2.html",
         img: "img/Дюрарара!! x2.jpg",
-        main: false
+        main: 3
     },
     {
         name: "Мириады цветов фантомного мира",
         URL: "https://www.anilibria.tv/release/musaigen-no-phantom-world.html",
         img: "img/Мириады цветов фантомного мира.jpg",
-        main: false
+        main: 3
     },
     {
         name: "Волчица и пряности: Торговец встречает мудрую волчицу",
         URL: "https://www.anilibria.tv/release/ookami-to-koushinryou-merchant-meets-the-wise-wolf.html",
         img: "img/Торговец встречает мудрую волчицу.jpg",
-        main: false
+        main: 3
     },
     {
         name: "Одна комната, солнечный свет, ангел",
         URL: "https://www.anilibria.tv/release/one-room-hiatari-futsuu-tenshi-tsuki.html",
         img: "img/Одна комната, солнечный свет, ангел.jpg",
-        main: false
+        main: 3
     },
     {
         name: "Добро пожаловать в класс превосходства",
         URL: "https://www.anilibria.tv/release/youkoso-jitsuryoku-shijou-shugi-no-kyoushitsu-e.html",
         img: "img/Добро пожаловать в класс превосходства.jpg",
-        main: false
+        main: 3
     },
 
     {
         name: "Я, владыка демонов, взял эльфийку-рабыню в жёны. ",
         URL: "https://www.anilibria.tv/release/maou-no-ore-ga-dorei-elf-wo-yome-ni-shitanda-ga-dou-medereba-ii.html",
         img: "img/взял эльфийку-рабыню в жёны.jpg",
-        main: false
+        main: 3
     },
     {
         name: "Адский рай",
         URL: "https://www.anilibria.tv/release/jigokuraku.html",
         img: "img/Адский рай.jpg",
-        main: false
+        main: 3
     },
     {
         name: "Перевоплотился в седьмого принца, так что я буду совершенствовать свою магию, как захочу",
         URL: "https://www.anilibria.tv/release/tensei-shitara-dainana-ouji-datta-node-kimama-ni-majutsu-wo-kiwamemasu.html",
         img: "img/Перевоплотился в седьмого принца так что я буду совершенствовать свою магию, как захочу.jpg",
-        main: false
+        main: 3
     },
     {
         name: "Лунное путешествие приведёт к новому миру 2",
         URL: "https://www.anilibria.tv/release/tsuki-ga-michibiku-isekai-douchuu.html",
         img: "img/Лунное путешествие приведёт к новому миру 2.jpg",
-        main: 1
+        main: 3
     },
     {
         name: "Мир по-прежнему красив",
         URL: "https://anilibria.today/anime/mir-po-prezhnemu-krasiv.html",
         img: "img/Мир по-прежнему красив.webp",
-        main: 2
+        main: 3
     },
     {
         name: "Ангел по соседству",
@@ -231,24 +231,35 @@ let pages = [
 //не трогать
 
 pages.forEach(anime => {
-    let animeList = document.querySelectorAll(".anime-list");
-    animeList.forEach(list => {
-        if (list.dataset.main == anime.main) {
-            let animeDiv = document.createElement("div");
-            let animeLink = document.createElement("a");
-            let animeTitle = document.createElement("h3");
-            let animeImg = document.createElement("img");
-            animeDiv.classList.add("element");
-            animeTitle.textContent = anime.name;
+    let animeList = null;
+    if (anime.main == 1) {
+        animeList = document.getElementById("lents");
+    } else if (anime.main == 2) {
+        animeList = document.getElementById("watch_lents");
+    } else if (anime.main == 3) {
+        animeList = document.getElementById("soon_lents");
+    }
 
-            animeImg.src = anime.img;
-            animeImg.alt = anime.name;
-            animeLink.href = anime.URL;
+    // Проверка, что элемент найден
+    if (!animeList) {
+        console.warn(`No element found for anime with main value ${anime.main}`);
+        return;
+    }
 
-            animeDiv.appendChild(animeLink);
-            list.appendChild(animeDiv);
-            animeLink.appendChild(animeImg);
-            animeLink.appendChild(animeTitle);
-        }
-    });
+    let animeDiv = document.createElement("div");
+    let animeLink = document.createElement("a");
+    let animeTitle = document.createElement("h3");
+    let animeImg = document.createElement("img");
+
+    animeDiv.classList.add("element");
+    animeTitle.textContent = anime.name;
+    animeImg.src = anime.img;
+    animeImg.alt = anime.name;
+    animeLink.href = anime.URL;
+
+    animeDiv.appendChild(animeLink);
+    animeList.appendChild(animeDiv);
+    animeLink.appendChild(animeImg);
+    animeLink.appendChild(animeTitle);
+});
 });
